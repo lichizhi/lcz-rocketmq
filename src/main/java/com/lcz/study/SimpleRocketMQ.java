04e0068754e5864ad275dfe10420f93d71544683
1591844498@qq.com <1591844498@qq.com>
2022-11-15 14:22:02 +0800

    1

--- src/main/java/com/lcz/study/SimpleRocketMQ.java
+++ src/main/java/com/lcz/study/SimpleRocketMQ.java
@@ -10,11 +10,14 @@ import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.common.protocol.body.ClusterInfo;
 import org.apache.rocketmq.common.protocol.body.TopicList;
+import org.apache.rocketmq.common.protocol.route.BrokerData;
 import org.apache.rocketmq.common.protocol.route.TopicRouteData;
 import org.apache.rocketmq.tools.admin.DefaultMQAdminExt;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
@@ -36,6 +39,7 @@ public class SimpleRocketMQ {
             message.setTopic("wula");
             message.setTags("tagA");
             message.setBody(("wula_" + i).getBytes());
+//            message.setWaitStoreMsgOK(true); // 约等于kafka的 ack=0
 
             // 1.同步发送
 //            SendResult sendResult = producer.send(message);
@@ -72,11 +76,17 @@ public class SimpleRocketMQ {
 //            producer.shutdown();
     }
 
+    /**
+     * rocketmq的broker服务端自己会维护重试队列和死信队列，以topic、consumerGroup的形式
+     * %RETRY%c_ooxx
+     * %DLQ%c_ooxx
+     */
     @Test
     public void consumerPush() throws Exception {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("c_ooxx");
         consumer.setNamesrvAddr("rocketmqA:9876");
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+//        consumer.setMaxReconsumeTimes(2);
 
         // 订阅
         consumer.subscribe("wula", "*");
@@ -87,6 +97,7 @@ public class SimpleRocketMQ {
                 System.out.println(new String(messageExt.getBody()));
             });
 
+            // ConsumeConcurrentlyStatus.RECONSUME_LATER  等会重新消费。时间是递增的。默认最大18次
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
 
@@ -99,15 +110,23 @@ public class SimpleRocketMQ {
     public void admin() throws Exception {
 
         DefaultMQAdminExt admin = new DefaultMQAdminExt();
-        admin.setNamesrvAddr("rocketmqA:9876");
+        admin.setNamesrvAddr("rocketmqA:9876;rocketmqB:9876;rocketmqC:9876");
         admin.start();
 
-        TopicList topicList = admin.fetchAllTopicList();
-        Set<String> list = topicList.getTopicList();
-        System.out.println(list);
+        ClusterInfo clusterInfo = admin.examineBrokerClusterInfo();
+        HashMap<String, BrokerData> brokerAddrTable = clusterInfo.getBrokerAddrTable();
+        brokerAddrTable.forEach(((s, brokerData) -> {
+            System.out.println("broker name: " + s);
+            System.out.println("broker data: " + brokerData);
+            System.out.println("===============");
+        }));
 
-        TopicRouteData wula = admin.examineTopicRouteInfo("wula");
-        System.out.println(wula);
+//        TopicList topicList = admin.fetchAllTopicList();
+//        Set<String> list = topicList.getTopicList();
+//        System.out.println(list);
+//
+//        TopicRouteData wula = admin.examineTopicRouteInfo("wula");
+//        System.out.println(wula);
 
     }
 
